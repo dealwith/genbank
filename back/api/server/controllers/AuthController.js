db04d468910651a3caa7c766bc7f4ee0371678f1
Gleb <krishin@inbox.ru>
2019-11-17 01:44:32 +0300

    deleted unused imports

--- back/api/server/controllers/AuthController.js
+++ back/api/server/controllers/AuthController.js
@@ -1,6 +1,5 @@
 import AuthServices from "../services/AuthServices";
 import Util from "../utils/Utils";
-import { argon2d } from "argon2";
 
 const util = new Util();
 
@@ -20,10 +19,8 @@ export default class AuthController {
 
   static async signIn(req, res) {
     const { email, password } = req.body;
-    console.log(email, password);
     try {
       const loggedInUser = await AuthServices.signIn(email, password);
-      console.log(loggedInUser);
       util.setSuccess(200, "Successfully logged in", loggedInUser);
       return util.send(res);
     } catch (error) {
