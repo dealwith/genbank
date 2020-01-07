cc1a8bd9b8ebfc2c22f7bc82222b21dbd765fe76
Gleb <krishin@inbox.ru>
2020-01-07 17:58:27 +0300

    Refactor Notification to own component

--- front/src/components/App/App.jsx
+++ front/src/components/App/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "regenerator-runtime/runtime";
 import { Router, Route, Redirect, Switch } from "react-router-dom";
-import { connect } from "react-redux";
 
 import { Header } from "../Header";
 import { Main } from "../Main";
@@ -13,63 +12,26 @@ import { Login } from "../Auth";
 import { Registrate } from "../Auth";
 import { Footer } from "../Footer";
 import { Users } from "../Users";
+import { Notification } from "../Notification";
 
-import { alertActions } from "../../actions";
 import { history } from "../../helpers";
 
-const App = props => {
-  const [show, setShow] = useState([show]);
-  const { alert } = props;
-
-  history.listen((location, action) => {
-    props.clearAlerts();
-  });
-
-  return (
-    <Router history={history}>
-      <Aside />
-      <Header />
-      <Main>
-        {alert.message && (
-          <div className={`alert g-alert ${alert.type}`}>
-            {alert.message}
-            <button
-              className="g-alert__close"
-              aria-labelledby="Close Notification"
-            >
-              <span className="g-hide-visually">Close</span>
-              <svg className="g-alert__close-icon" viewBox="0 0 40 40">
-                <path d="M 10,10 L 30,30 M 30,10 L 10,30"></path>
-              </svg>
-            </button>
-          </div>
-        )}
-        <Switch>
-          <Route path="/" exact component={BasicTable} />
-          <Route path="/species/:speciesId" component={ExactSpeciesTable} />
-          <Route path="/add-species" component={AddSpeciesForm} />
-          <Route path="/login" component={Login} />
-          <Route path="/registrate" component={Registrate} />
-          <Route path="/users" component={Users} />
-          <Redirect from="*" to="/" />
-        </Switch>
-        <Footer />
-      </Main>
-    </Router>
-  );
-};
-
-function mapState(state) {
-  const { alert } = state;
-  return { alert };
-}
-
-const actionCreators = {
-  clearAlerts: alertActions.clear
-};
-
-const connectedApp = connect(
-  mapState,
-  actionCreators
-)(App);
-export { connectedApp as App };
+export const App = props => (
+  <Router history={history}>
+    <Aside />
+    <Header />
+    <Main>
+      <Notification />
+      <Switch>
+        <Route path="/" exact component={BasicTable} />
+        <Route path="/species/:speciesId" component={ExactSpeciesTable} />
+        <Route path="/add-species" component={AddSpeciesForm} />
+        <Route path="/login" component={Login} />
+        <Route path="/registrate" component={Registrate} />
+        <Route path="/users" component={Users} />
+        <Redirect from="*" to="/" />
+      </Switch>
+      <Footer />
+    </Main>
+  </Router>
+);
---
+++ front/src/components/Notification/Notification.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { history } from "../../helpers";
+import { alertActions } from "../../actions";
+
+const Notification = props => {
+  const { alert } = props;
+
+  history.listen((location, action) => {
+    props.clearAlerts();
+  });
+
+  return (
+    <>
+      {alert.message && (
+        <div className={`alert g-alert ${alert.type}`}>
+          {alert.message}
+          <button
+            className="g-alert__close"
+            aria-labelledby="Close Notification"
+          >
+            <span className="g-hide-visually">Close</span>
+            <svg className="g-alert__close-icon" viewBox="0 0 40 40">
+              <path d="M 10,10 L 30,30 M 30,10 L 10,30"></path>
+            </svg>
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
+
+function mapState(state) {
+  const { alert } = state;
+  return { alert };
+}
+
+const actionCreators = {
+  clearAlerts: alertActions.clear
+};
+
+const connectedNotification = connect(mapState, actionCreators)(Notification);
+export { connectedNotification as Notification };
