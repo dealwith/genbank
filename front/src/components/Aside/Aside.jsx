2986adf7655ce1f53ad4ecf4f4b95652a4667994
Gleb <krishin@inbox.ru>
2021-01-04 20:53:38 +0300

    Change aside

--- front/src/components/Aside/Aside.jsx
+++ front/src/components/Aside/Aside.jsx
@@ -1,22 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Image } from "../Image";
+
+import logo from "../../public/images/NAS-logo.jpg";
 
 export const Aside = () => (
   <aside className="g-aside">
-    <div className="g-aside__header">
-      <h1 className="g-text-logo">Logo</h1>
+    <div className="g-aside__header pt-3">
+      <div className="g-aside__header-logo">
+        <Image
+          src={logo}
+          alt="logo"
+          fluid="true"
+          size={{ width: 200 }}
+          className="g-aside__header-logo-img"
+        />
+      </div>
     </div>
     <div className="g-aside__main">
       <nav className="g-nav g-nav-admin">
         <ul className="g-text-invert">
           <li>
-            <Link to="/users">users</Link>
+            <Link to="/users">Пользователи</Link>
           </li>
           <li>
-            <Link to="/">main</Link>
+            <Link to="/">Главная</Link>
           </li>
           <li>
-            <Link to="/add-species">add species</Link>
+            <Link to="/add-species">Добавить вид</Link>
           </li>
         </ul>
       </nav>
