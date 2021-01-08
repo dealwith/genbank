307aeb28dbec04e7a32ad0da5264428205ae53f2
Gleb <krishin@inbox.ru>
2021-01-08 04:09:46 +0300

    Change getPagination method

--- back/api/server/helpers/getPagination.js
+++ back/api/server/helpers/getPagination.js
@@ -1,6 +1,6 @@
 export const getPagination = (page = 0, size = 10) => {
-	const limit = size;
-	const offset = page * limit;
+	const limit = size ? +size : 10;
+	const offset = page ? page * limit : 0;
 
 	return { limit, offset };
 }
