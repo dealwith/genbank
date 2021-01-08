9ec4f8793c0f4dced2f1a290d4f9b48d8772ad93
Gleb <krishin@inbox.ru>
2021-01-08 04:16:30 +0300

    Add plug for users tab

--- front/src/components/Users/Users.jsx
+++ front/src/components/Users/Users.jsx
@@ -1,12 +1,6 @@
 import React from "react";
-import { Link, Button, Form } from "react-bootstrap";
 
 export const Users = () => {
-  const [user, setUser] = useState([]);
-  useEffect(() => {
-    const fetchData = () => {};
-    return () => {};
-  }, []);
   return (
     <>
       <ul></ul>
