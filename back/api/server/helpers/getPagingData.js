f8db2525c4db44019c30a774355036143cf537ab
Gleb <krishin@inbox.ru>
2021-01-08 04:12:02 +0300

    Add comment for easier understanding of Math.floor instead of Math.ceil in code

--- back/api/server/helpers/getPagingData.js
+++ back/api/server/helpers/getPagingData.js
@@ -1,6 +1,8 @@
 export const getPagingData = (limitedSpecies, page, limit) => {
 	const { count: totalItems, rows: species } = limitedSpecies;
 	const currentPage = page ? +page : 0;
+	// Math floor for easier work on front-end with rendering items,
+	// because counting in arrays and loops always start from 0
 	const totalPages = Math.floor(totalItems / limit);
 
 	return { totalPages, currentPage, totalItems, species }
